f457a5540f33ba690ab1fbb33914dfe14355a8dc
milka <mileto94@abv.bg>
2014-07-08 13:54:22 +0300

    Functional test checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -12,7 +13,28 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_with_wrong_title(self):
         self.browser.get("http://localhost:8000")
-        self.assertIn("To do", self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", self.browser.title)
+        self.assertIn("To-Do", header_text)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
+
+        # Try entering someting
+        inputbox.send_keys("Buy some chocolate")
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = self.browser.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: Buy some chocolate" for row in rows)
+        )
+
         self.fail("Finish the test")
 
 if __name__ == '__main__':
