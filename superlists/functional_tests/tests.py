a3867a57efb22ee77225a4be1fac3fe5ee993fa9
milka <mileto94@abv.bg>
2014-07-09 14:30:22 +0300

    Add new test for entering a second user

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -23,6 +23,9 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn("To-Do", self.browser.title)
         self.assertIn("To-Do", header_text)
 
+    def test_entering_second_person(self):
+        self.browser.get(self.live_server_url)
+
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertEqual(
             inputbox.get_attribute("placeholder"),
@@ -31,11 +34,32 @@ class NewVisitorTest(LiveServerTestCase):
         # Try entering someting
         inputbox.send_keys("Buy some chocolate")
         inputbox.send_keys(Keys.ENTER)
+        milkas_list_url = self.browser.current_url
+        self.assertRegex(milkas_list_url, "/lists/.+")
         self.check_for_row_in_list_table("1: Buy some chocolate")
 
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys("Eat the chocolate")
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table("2: Eat the chocolate")
+
+        self.browser.quit()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy some chocolate", page_text)
+        self.assertNotIn("Eat the chocolate", page_text)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+
+        emmas_list_url = self.browser.current_url
+        self.assertRegex(emmas_list_url, "/lists/.+")
+        self.assertNotEqual(emmas_list_url, milkas_list_url)
+
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Buy some chocolate", page_text)
+        self.assertIn("Buy milk", page_text)
 
         self.fail("Finish the test")
