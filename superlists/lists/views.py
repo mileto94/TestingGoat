177a509abae560bbfc5cf302c7ccab64d1abe73b
milka <mileto94@abv.bg>
2014-07-09 12:17:05 +0300

    Add template to home_page+tests

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
 
 
@@ -6,9 +6,6 @@ def home_page(request):
     if request.method == "POST":
         new_item_text = request.POST["item_text"]
         Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ""
-
-    return render(request, "home.html", {
-        "new_item_text": new_item_text,
-    })
+        return redirect("/")
+    items = Item.objects.all()
+    return render(request, "home.html", {"items": items})
