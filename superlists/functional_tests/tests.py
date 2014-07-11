88f150c86f2866e474ef207ec803e71dbd6b37d7
milka <mileto94@abv.bg>
2014-07-11 17:58:47 +0300

    Refactor home.html and lists.hmtl to use base.html

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -61,4 +61,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn("Buy some chocolate", page_text)
         self.assertIn("Buy milk", page_text)
 
-        self.fail("Finish the test")
+        # self.fail("Finish the test")
+
+    def test_styling_and_layout(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # inputbox.send_keys("testing\n")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=5
+        )
