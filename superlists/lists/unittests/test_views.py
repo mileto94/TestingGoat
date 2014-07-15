39196af5ebd36e4210520189e7977d61ae080eee
milka <mileto94@abv.bg>
2014-07-15 16:50:29 +0300

    Separate unittests

--- superlists/lists/unittests/test_views.py
+++ superlists/lists/unittests/test_views.py
@@ -4,7 +4,6 @@ from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
 from django.utils.html import escape
 
 
@@ -18,42 +17,6 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string("home.html")
         self.assertEqual(response.content.decode(), expected_html)
-
-
-class ListsAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "First list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Second item"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "First list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "Second item")
-        self.assertEqual(second_saved_item.list, list_)
-
-    def test_cannot_save_empty_list_item(self):
-        list_ = List.objects.create()
-        item = Item(list=list_, text="")
-        with self.assertRaises(ValidationError):
-            item.save()
-            item.full_clean()
 
 
 class ListViewTest(TestCase):
