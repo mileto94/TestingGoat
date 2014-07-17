aa704746cb1e547392ae8abc6fcff1ea7246be58
milka <mileto94@abv.bg>
2014-07-17 16:22:50 +0300

    Add new test+function(using DRY)

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -2,11 +2,14 @@ from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector(".has-error")
+
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys("\n")
 
-        error = self.browser.find_element_by_css_selector(".has-error")
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
     def test_cannot_add_empty_list_item_after_excisting_item(self):
@@ -15,7 +18,7 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table("1: Buy milk")
 
         self.get_item_input_box().send_keys("\n")
-        error = self.browser.find_element_by_css_selector(".has-error")
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
     def test_cannot_add_duplicate_items(self):
@@ -26,5 +29,15 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys("Buy milk\n")
         self.check_for_row_in_list_table("1: Buy milk")
 
-        error = self.browser.find_element_by_css_selector(".has-error")
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("\n")
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        self.get_item_input_box().send_keys("Halo")
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
